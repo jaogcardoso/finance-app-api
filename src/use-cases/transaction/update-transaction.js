489f9d6a0c406bbad7ef068cef4b9893df2d0be4
João <joaogabrielcardoso8@gmail.com>
2026-02-09 09:10:41 -0300

    Feat: create update transacion use case

--- src/use-cases/transaction/update-transaction.js
+++ src/use-cases/transaction/update-transaction.js
@@ -1,22 +1,20 @@
-import {UserNotFoundError} from '../../errors/user.js'
+import { UserNotFoundError } from '../../errors/user.js'
 
 export class UpdateTransactionUseCase {
-    constructor(updateTransactionRepository, getUserByIdRepository){
-        this.updateTransactionRepository = new updateTransactionRepository
-        this.getUserByIdRepository = new getUserByIdRepository
+  constructor(updateTransactionRepository, getUserByIdRepository) {
+    this.updateTransactionRepository = new updateTransactionRepository()
+    this.getUserByIdRepository = new getUserByIdRepository()
+  }
+
+  async execute(params) {
+    const user = await this.getUserByIdRepository.execute(params.userId)
+
+    if (!user) {
+      throw new UserNotFoundError()
     }
 
-    async execute (params) {
-        const user = await this.getUserByIdRepository.execute(params.userId)
+    const transaction = await this.updateTransactionRepository.execute(params)
 
-        if (!user) {
-            throw new UserNotFoundError
-        }
-
-        const transaction = await this.updateTransactionRepository.execute(params)
-
-        return transaction
-    }
-
-
+    return transaction
+  }
 }
