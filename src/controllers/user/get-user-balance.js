424e09802db0699c550df10624e04bc3dc0aeadf
João <joaogabrielcardoso8@gmail.com>
2026-02-10 14:09:19 -0300

    checkIfIdIsValid, invalidIdResponse, ok

---
+++ src/controllers/user/get-user-balance.js
@@ -0,0 +1,32 @@
+import { UserNotFoundError } from '../../errors/user.js'
+import { serverError, userNotFoundResponse, checkIfIdIsValid, invalidIdResponse, ok } from '../helpers.js'
+
+export class GetUserController {
+  constructor(getUserBalanceUseCase) {
+    this.getUserBalanceUseCase = getUserBalanceUseCase
+  }
+
+  async execute(httpRequest) {
+    try {
+      const userId = httpRequest.params.userId
+
+      const idIsValid = checkIfIdIsValid(userId)
+
+      if(!idIsValid) {
+        return invalidIdResponse()
+      }
+
+      const balance = await this.getUserBalanceUseCase.execute({userId})
+
+      return ok(balance)
+
+    } catch (error) {
+      if (error instanceof UserNotFoundError) {
+        return userNotFoundResponse()
+      }
+
+      console.error(error)
+      return serverError()
+    }
+  }
+}
