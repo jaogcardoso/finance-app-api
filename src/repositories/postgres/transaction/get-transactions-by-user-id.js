afaef406a3a441310039b030fa1a91c9e81f8cc9
João <joaogabrielcardoso8@gmail.com>
2026-02-18 13:31:31 -0300

    Feat: add update transaction and get transaction by user on prisma

--- src/repositories/postgres/transaction/get-transactions-by-user-id.js
+++ src/repositories/postgres/transaction/get-transactions-by-user-id.js
@@ -1,13 +1,12 @@
 import { prisma } from '../../../../prisma/prisma.js'
-import { PostgresHelper } from '../../../db/postgres/client.js'
 
-export class PostgresGetTransactionsByUserIdRepository{
-    async execute(userId) {
-        return await prisma.transaction
-        const transactions = await PostgresHelper.query('SELECT * FROM transactions WHERE user_id = $1',
-            [userId]
-        )
 
-        return transactions
-    }
+export class PostgresGetTransactionsByUserIdRepository {
+  async execute(userId) {
+    return await prisma.transaction.findMany({
+      where: {
+        user_id: userId,
+      },
+    })
+  }
 }
--- src/repositories/postgres/transaction/update-transaction.js
+++ src/repositories/postgres/transaction/update-transaction.js
@@ -1,21 +1,12 @@
-import { PostgresHelper } from '../../../db/postgres/client.js'
+import { prisma } from '../../../../prisma/prisma.js'
 
 export class PostgresUpdateTransactionRepository {
   async execute(transactionId, updateTransactionParams) {
-    const updateFields = []
-    const updateValues = []
-
-    Object.keys(updateTransactionParams).forEach((key) => {
-      updateFields.push(`${key} = $${updateValues.length + 1}`)
-      updateValues.push(updateTransactionParams[key])
+    return await prisma.transaction.update({
+      where: {
+        id: transactionId,
+      },
+      data: updateTransactionParams,
     })
-
-    updateValues.push(transactionId)
-
-    const updateQuery = `UPDATE transactions SET ${updateFields.join(', ')} WHERE id = $${updateValues.length} RETURNING *`
-
-    const updateUser = await PostgresHelper.query(updateQuery, updateValues)
-
-    return updateUser[0]
   }
 }
