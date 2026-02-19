1263d24d1ddac99d9fedd268d7e6f94b672eec5f
João <joaogabrielcardoso8@gmail.com>
2026-02-19 14:18:55 -0300

    Feat: add jwt token and create login use case

---
+++ src/use-cases/user/login-user.js
@@ -0,0 +1,29 @@
+import { InvalidPasswordError, UserNotFoundError } from '../../errors/user.js'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+
+export class LoginUserUseCase {
+  constructor(getUserByEmailRepository) {
+    this.getUserByEmailRepository = getUserByEmailRepository
+  }
+  async execute(email, password) {
+    const user = await this.getUserByEmailRepository.execute(email)
+
+    if (!user) {
+      throw new UserNotFoundError()
+    }
+
+    const isPasswordValid = bcrypt.compare(password, user.password)
+    if (!isPasswordValid) {
+        throw new InvalidPasswordError
+    }
+    const tokens = {
+        accessToken: jwt.sign({userId: user.id}, process.env.JWT_ACCESS_TOKEN_SECRET,{expiresIn:'15m'},),
+        refreshToken: jwt.sign({userId: user.id}, process.env.JWT_REFRESH_TOKEN_SECRET,{expiresIn: '1d'})
+    }
+    return {
+        ...user,
+        tokens
+    }
+  }
+}
